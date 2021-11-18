35f220addea016df0c28b837835a10a062a89541
GilevskiA <93896513+GilevskiA@users.noreply.github.com>
2021-11-18 13:01:44 +1100

    updated shopping_list.php 18/11 1:01PM
    changed variable names to match with other files.

--- shopping_list.php
+++ shopping_list.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html> <!--Basic HTML Formatting-->
 <html>
    <head>
-      <title>Task List Manager</title>
+      <title>Item List Manager</title>
       <link rel="stylesheet" type="text/css" href="main.css"> <!--Linking Style Sheet-->
    </head>
    <body>
       <header>
-         <h1> Task List Manager </h1>
+         <h1>Item List Manager</h1>
       </header>
       <main>
-         <p><?php print_r($task_list); ?></p> <!--Displays array-->
+         <p><?php print_r($shopping_list); ?></p> <!--Displays array-->
 		 
 		 
          <!-- Part 1: Errors -->
@@ -24,15 +24,15 @@
          <?php endif; ?>
 		 
 		 
-         <!-- Part 2: Tasks -->
-         <h2>Tasks:</h2>
+         <!-- Part 2: Items -->
+         <h2>items:</h2>
 		 
-         <?php if (count($task_list) == 0) : ?> <!--If there are no tasks, message below will appear-->
-         <p>There are no tasks in the task list.</p>
+         <?php if (count($shopping_list) == 0) : ?> <!--If there are no items, message below will appear-->
+         <p>There are no items in the item list.</p>
          <?php else: ?>
          <ul>
-            <?php foreach( $task_list as $id => $task ) : ?>
-            <li><?php echo $id + 1 . '. ' . $task; ?></li>
+            <?php foreach( $shopping_list as $id => $item ) : ?>
+            <li><?php echo $id + 1 . '. ' . $item; ?></li>
             <?php endforeach; ?>
          </ul>
          <?php endif; ?>
@@ -40,65 +40,65 @@
 		 
 		 
          <!-- Part 3: The Add form -->
-         <h2>Add Task:</h2>
+         <h2>Add item:</h2>
 		 
          <form action="." method="post" >
-            <?php foreach( $task_list as $task ) : ?>
-            <input type="hidden" name="tasklist[]"
-               value="<?php echo $task; ?>">
+            <?php foreach( $shopping_list as $item ) : ?>
+            <input type="hidden" name="itemlist[]"
+               value="<?php echo $item; ?>">
             <?php endforeach; ?>
-            <label>Task:</label>
-            <input type="text" name="newtask" id="newtask"> <br>
+            <label>item:</label>
+            <input type="text" name="newitem" id="newitem"> <br>
             <label>&nbsp;</label>
-            <input type="submit" name="action" value="Add Task">
+            <input type="submit" name="action" value="Add item">
          </form>
          <br>
 		 
 		 
          <!-- Part 4: The Modify/Promote/Delete form -->
-         <?php if (count($task_list) > 0 && empty($task_to_modify)) : ?>
+         <?php if (count($shopping_list) > 0 && empty($shopping_to_modify)) : ?>
 		 
-         <h2>Select Task:</h2>
+         <h2>Select item:</h2>
 		 
          <form action="." method="post" >
-            <?php foreach( $task_list as $task ) : ?>
-            <input type="hidden" name="tasklist[]"
-               value="<?php echo $task; ?>">
+            <?php foreach( $shopping_list as $item ) : ?>
+            <input type="hidden" name="itemlist[]"
+               value="<?php echo $item; ?>">
             <?php endforeach; ?>
 			
-            <label>Task:</label>
+            <label>item:</label>
 			
-            <select name="taskid">
-               <?php foreach( $task_list as $id => $task ) : ?>
+            <select name="itemid">
+               <?php foreach( $shopping_list as $id => $item ) : ?>
                <option value="<?php echo $id; ?>">
-                  <?php echo $task; ?>
+                  <?php echo $item; ?>
                </option>
                <?php endforeach; ?>
             </select>
             <br>
             <label>&nbsp;</label>
-            <input type="submit" name="action" value="Modify Task">
-            <input type="submit" name="action" value="Promote Task">
-            <input type="submit" name="action" value="Delete Task">
+            <input type="submit" name="action" value="Modify item">
+            <input type="submit" name="action" value="Promote item">
+            <input type="submit" name="action" value="Delete item">
             <br>
             <label>&nbsp;</label>
-            <input type="submit" name="action" value="Sort Tasks">
+            <input type="submit" name="action" value="Sort items">
          </form>
          <?php endif; ?>
 		 
 		 
          <!-- Part 5: The Modify form -->
-         <?php if (!empty($task_to_modify)) : ?>
+         <?php if (!empty($shopping_to_modify)) : ?>
 		 
-         <h2>Task to Modify:</h2>
+         <h2>item to Modify:</h2>
 		 
          <form action="." method="post" >
-            <?php foreach( $task_list as $task ) : ?>
-            <input type="hidden" name="tasklist[]" value="<?php echo $task; ?>">
+            <?php foreach( $shopping_list as $item ) : ?>
+            <input type="hidden" name="itemlist[]" value="<?php echo $item; ?>">
             <?php endforeach; ?>
-            <label>Task:</label>
-            <input type="hidden" name="modifiedtaskid" value="<?php echo $task_index; ?>">
-            <input type="text" name="modifiedtask" value="<?php echo $task_to_modify; ?>"><br>
+            <label>item:</label>
+            <input type="hidden" name="modifieditem" value="<?php echo $item_index; ?>">
+            <input type="text" name="modifieditem" value="<?php echo $shopping_to_modify; ?>"><br>
             <label>&nbsp;</label>
             <input type="submit" name="action" value="Save Changes">
             <input type="submit" name="action" value="Cancel Changes">
