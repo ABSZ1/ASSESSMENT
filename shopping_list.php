48bbf67d291ad99b77c9f1435ba847c3f5fb0e7e
ABSZ1 <93896332+ABSZ1@users.noreply.github.com>
2021-11-19 15:36:30 +1100

    Update shopping_list.php

--- shopping_list.php
+++ shopping_list.php
@@ -1,109 +1,97 @@
-<!DOCTYPE html> <!--Basic HTML Formatting-->
+<!DOCTYPE html>
 <html>
-   <head>
-      <title>Item List Manager</title>
-      <link rel="stylesheet" type="text/css" href="main.css"> <!--Linking Style Sheet-->
-   </head>
-   <body>
-      <header>
-         <h1>Item List Manager</h1>
-      </header>
-      <main>
-         <p><?php print_r($shopping_list); ?></p> <!--Displays array-->
-		 
-		 
-         <!-- Part 1: Errors -->
-         <?php if (count($errors) > 0) : ?> <!--If an error is detected, message below will be executed-->
-         <h2>Error/s:</h2>
-		 
-         <ul>
-            <?php foreach($errors as $error) : ?>
-            <li><?php echo $error; ?></li>
-            <?php endforeach; ?>
-         </ul>
-         <?php endif; ?>
-		 
-		 
-         <!-- Part 2: Items -->
-         <h2>items:</h2>
-		 
-         <?php if (count($shopping_list) == 0) : ?> <!--If there are no items, message below will appear-->
-         <p>There are no items in the item list.</p>
-         <?php else: ?>
-         <ul>
-            <?php foreach( $shopping_list as $id => $item ) : ?>
-            <li><?php echo $id + 1 . '. ' . $item; ?></li>
-            <?php endforeach; ?>
-         </ul>
-         <?php endif; ?>
-         <br>
-		 
-		 
-         <!-- Part 3: The Add form -->
-         <h2>Add item:</h2>
-		 
-         <form action="." method="post" >
-            <?php foreach( $shopping_list as $item ) : ?>
-            <input type="hidden" name="itemlist[]"
-               value="<?php echo $item; ?>">
-            <?php endforeach; ?>
-            <label>item:</label>
-            <input type="text" name="newitem" id="newitem"> <br>
-            <label>&nbsp;</label>
-            <input type="submit" name="action" value="Add item">
-         </form>
-         <br>
-		 
-		 
-         <!-- Part 4: The Modify/Promote/Delete form -->
-         <?php if (count($shopping_list) > 0 && empty($shopping_to_modify)) : ?>
-		 
-         <h2>Select item:</h2>
-		 
-         <form action="." method="post" >
-            <?php foreach( $shopping_list as $item ) : ?>
-            <input type="hidden" name="itemlist[]"
-               value="<?php echo $item; ?>">
-            <?php endforeach; ?>
-			
-            <label>item:</label>
-			
-            <select name="itemid">
-               <?php foreach( $shopping_list as $id => $item ) : ?>
-               <option value="<?php echo $id; ?>">
-                  <?php echo $item; ?>
-               </option>
-               <?php endforeach; ?>
-            </select>
-            <br>
-            <label>&nbsp;</label>
-            <input type="submit" name="action" value="Modify item">
-            <input type="submit" name="action" value="Promote item">
-            <input type="submit" name="action" value="Delete item">
-            <br>
-            <label>&nbsp;</label>
-            <input type="submit" name="action" value="Sort items">
-         </form>
-         <?php endif; ?>
-		 
-		 
-         <!-- Part 5: The Modify form -->
-         <?php if (!empty($shopping_to_modify)) : ?>
-		 
-         <h2>item to Modify:</h2>
-		 
-         <form action="." method="post" >
-            <?php foreach( $shopping_list as $item ) : ?>
-            <input type="hidden" name="itemlist[]" value="<?php echo $item; ?>">
-            <?php endforeach; ?>
-            <label>item:</label>
-            <input type="hidden" name="modifieditem" value="<?php echo $item_index; ?>">
-            <input type="text" name="modifieditem" value="<?php echo $shopping_to_modify; ?>"><br>
-            <label>&nbsp;</label>
-            <input type="submit" name="action" value="Save Changes">
-            <input type="submit" name="action" value="Cancel Changes">
-         </form>
-         <?php endif; ?>
-      </main>
-   </body>
+<head>
+<title>Shopping List Manager</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<header>
+<h1>Shopping List Manager</h1>
+</header>
+<main>
+<p><?php print_r($shopping_list); ?></p>
+
+<!-- part 1: the errors -->
+<?php if (count($errors) > 0) : ?>
+<h2>Errors:</h2>
+<ul>
+<?php foreach($errors as $error) : ?>
+<li><?php echo $error; ?></li>
+<?php endforeach; ?>
+</ul>
+<?php endif; ?>
+
+<!-- part 2: the Item -->
+<h2>Item:</h2>
+<?php if (count($shopping_list) == 0) : ?>
+<p>There are no Item in the Shopping list.</p>
+<?php else: ?>
+<ul>
+<?php foreach( $shopping_list as $id => $item ) : ?>
+<li><?php echo $id + 1 . '. ' . $item; ?></li>
+<?php endforeach; ?>
+</ul>
+<?php endif; ?>
+<br>
+
+<!-- part 3: the add form -->
+<h2>Add Item:</h2>
+<form action="." method="post" >
+<?php foreach( $shopping_list as $item ) : ?>
+<input type="hidden" name="itemlist[]"
+value="<?php echo $item; ?>">
+<?php endforeach; ?>
+<label>Item:</label>
+<input type="text" name="newitem" id="newitem"> <br>
+<label>&nbsp;</label>
+<input type="submit" name="action" value="Add Item">
+</form>
+<br>
+
+<!-- part 4: the modify/promote/delete form -->
+<?php if (count($shopping_list) > 0 && empty($item_to_modify)) : ?>
+<h2>Select Item:</h2>
+<form action="." method="post" >
+<?php foreach( $shopping_list as $item ) : ?>
+<input type="hidden" name="itemlist[]"
+value="<?php echo $item; ?>">
+<?php endforeach; ?>
+<label>Item:</label>
+<select name="itemid">
+<?php foreach( $shopping_list as $id => $item ) : ?>
+<option value="<?php echo $id; ?>">
+<?php echo $item; ?>
+</option>
+<?php endforeach; ?>
+</select>
+<br>
+<label>&nbsp;</label>
+<input type="submit" name="action" value="Modify Item">
+<input type="submit" name="action" value="Promote Item">
+<input type="submit" name="action" value="Delete Item">
+
+<br>
+<label>&nbsp;</label>
+<input type="submit" name="action" value="Sort Item">
+</form>
+<?php endif; ?>
+
+<!-- part 5: the modify form -->
+<?php if (!empty($item_to_modify)) : ?>
+<h2>Item to Modify:</h2>
+<form action="." method="post" >
+<?php foreach( $shopping_list as $item ) : ?>
+<input type="hidden" name="itemlist[]" value="<?php echo $item; ?>">
+<?php endforeach; ?>
+<label>Item:</label>
+<input type="hidden" name="modifieditemid" value="<?php echo $item_index; ?>">
+<input type="text" name="modifiedtask" value="<?php echo $item_to_modify; ?>"><br>
+<label>&nbsp;</label>
+<input type="submit" name="action" value="Save Changes">
+<input type="submit" name="action" value="Cancel Changes">
+</form>
+<?php endif; ?>
+
+</main>
+</body>
 </html>
