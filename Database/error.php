76495ce7bbd04c8958f5ebcd1eb6d17f3a9329dc
GilevskiA <93896513+GilevskiA@users.noreply.github.com>
2021-11-21 11:45:44 +1100

    Update error.php
    - added a return button and fixed headings

--- Database/error.php
+++ Database/error.php
@@ -3,21 +3,23 @@
 
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>Shopping List Manager</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
 
 <!-- the body section -->
 <body>
-    <header><h1>My Guitar Shop</h1></header>
+    <header><h1>My Shopping List</h1></header>
 
     <main>
         <h2 class="top">Error</h2>
         <p><?php echo $error; ?></p>
+		<form method="POST" action="add_product_form.php">
+		    <input type="Submit" value="Return"/>
     </main>
 
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+        <p>&copy; <?php echo date("Y"); ?> Woolworths, Inc.</p>
     </footer>
 </body>
 </html>
