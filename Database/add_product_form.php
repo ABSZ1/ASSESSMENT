f6285a85e7208acdffd5571c5cdf745a583bb460
GilevskiA <93896513+GilevskiA@users.noreply.github.com>
2021-11-21 21:46:27 +1100

    Update add_product_form.php
    -Formatted code
    -Included comment

--- Database/add_product_form.php
+++ Database/add_product_form.php
@@ -1,5 +1,5 @@
 <?php
-require('database.php');
+require('database.php'); //Grab database.php
 $query = 'SELECT *
           FROM categories
           ORDER BY categoryID';
@@ -8,50 +8,53 @@ $statement->execute();
 $categories = $statement->fetchAll();
 $statement->closeCursor();
 ?>
+
 <!DOCTYPE html>
 <html>
 
-<!-- the head section -->
-<head>
-    <title>Shopping List</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-
-<!-- the body section -->
-<body>
-    <header><h1> Shopping List Product Manager</h1></header>
-
-    <main>
-        <h1>Add Product</h1>
-        <form action="add_product.php" method="post"
-              id="add_product_form">
-
+   <!--The Head section-->
+   <head>
+      <title>Shopping List</title>
+      <link rel="stylesheet" type="text/css" href="main.css">
+   </head>
+   
+   <!--The Body section-->
+   <body>
+   
+      <header>
+         <h1> Shopping List Manager</h1>
+      </header>
+	  
+      <main>
+         <h1>Add Product</h1>
+         <form action="add_product.php" method="post"
+            id="add_product_form">
             <label>Category:</label>
             <select name="category_id">
-            <?php foreach ($categories as $category) : ?>
-                <option value="<?php echo $category['categoryID']; ?>">
-                    <?php echo $category['categoryName']; ?>
-                </option>
-            <?php endforeach; ?>
-            </select><br>
-
+			
+               <?php foreach ($categories as $category) : ?>
+               <option value="<?php echo $category['categoryID']; ?>">
+                  <?php echo $category['categoryName']; ?>
+				  
+               </option>
+               <?php endforeach; ?>
+            </select>
+            <br>
             <label>Code:</label>
             <input type="text" name="code"><br>
-
             <label>Name:</label>
             <input type="text" name="name"><br>
-
             <label>List Price:</label>
             <input type="text" name="price"><br>
-
             <label>&nbsp;</label>
             <input type="submit" value="Add Product"><br>
-        </form>
-        <p><a href="index.php">View Product List</a></p>
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Woolworths Inc.</p>
-    </footer>
-</body>
+         </form>
+         <p><a href="index.php">View Product List</a></p>
+      </main>
+	  
+      <footer>
+         <p>&copy; <?php echo date("Y"); ?> Woolworths Inc.</p>
+      </footer>
+	  
+   </body>
 </html>
