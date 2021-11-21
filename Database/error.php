3ba7d608f4be504489515a5048f90b60793d03b6
GilevskiA <93896513+GilevskiA@users.noreply.github.com>
2021-11-21 21:30:56 +1100

    Update error.php
    -Formatted code

--- Database/error.php
+++ Database/error.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
 
-<!-- the head section -->
-<head>
-    <title>Shopping List Manager</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
-</head>
-
-<!-- the body section -->
-<body>
-    <header><h1>My Shopping List</h1></header>
-
-    <main>
-        <h2 class="top">Error</h2>
-        <p><?php echo $error; ?></p>
-		<form method="POST" action="add_product_form.php">
-		    <input type="Submit" value="Return"/>
-    </main>
-
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Woolworths, Inc.</p>
-    </footer>
-</body>
+   <!-- the head section -->
+   <head>
+      <title>Shopping List Manager</title>
+      <link rel="stylesheet" type="text/css" href="main.css" />
+   </head>
+   
+   <!-- the body section -->
+   <body>
+      <header>
+         <h1>Shopping List Manager</h1>
+      </header>
+      <main>
+         <h2 class="top">Error</h2>
+         <p><?php echo $error; ?></p>
+         <form method="POST" action="add_product_form.php">
+         <input type="Submit" value="Return"/>
+      </main>
+      <footer>
+         <p>&copy; <?php echo date("Y"); ?> Woolworths, Inc.</p>
+      </footer>
+   </body>
 </html>
