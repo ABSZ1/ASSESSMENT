4451434f3d019cd23924feb5415374cdae190856
GilevskiA <93896513+GilevskiA@users.noreply.github.com>
2021-11-21 21:41:57 +1100

    Update database.php
    -Formatted code
    -Added comments for better readability

--- Database/database.php
+++ Database/database.php
@@ -1,13 +1,16 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=Shopping_Database';
-    $username = 'root';
-    $password = '';
+$dsn = 'mysql:host=localhost;dbname=Shopping_Database'; //Database credentials to make it work
+$username = 'root';
+$password = '';
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
+try
+{
+    $db = new PDO($dsn, $username, $password);
+}
+catch(PDOException $e)
+{
+    $error_message = $e->getMessage(); //Execute database error message if anything wrong happens
+    include ('database_error.php');
+    exit();
+}
 ?>
